62aef608f5259ebc920207caecdbf7f3078e806a
nokk-01152 <2425401152@live.gctu.edu.gh>
2026-05-31 00:38:53 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,46 @@
+# File: initials.py
+# Description: print out my initials
+# Assignment Number: Assignment Number 2
+#
+# Name: NANA KWAME KORSAH OFORI 
+# STUDENT ID:  2425401152
+# Email: 2425401152@live.gctu.edu.gh
+# Grader: AUGUSTUS
+#
+# On my honor, NANA KWAME KORSAH OFORI, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
+
+
+
+
+
+
+
+
+
+
+
 
 def main():
-  pass #code goes here
+
+    print("...NOKK")
+    print(" ")
 
 
+    print("....NNN.NN..........NNN................OOO...............KKK......KKK..........KKK.....KKK..")
+    print("....NNN..NN.........NNN.............OOO...OOO............KKK.....KKK...........KKK....KKK...")
+    print("....NNN...NN........NNN...........OOO.......OOO..........KKK....KKK............KKK...KKK....")
+    print("....NNN....NN.......NNN..........OOO.........OOO.........KKK...KKK.............KKK..KKK.....")
+    print("....NNN.....NN......NNN.........OOO...........OOO........KKK.KK................KKK.KK.......")
+    print("....NNN......NN.....NNN.........OOO...........OOO........KKK..KKK..............KKK..KKK.....")
+    print("....NNN.......NN....NNN..........OOO.........OOO.........KKK...KKK.............KKK...KKK....")
+    print("....NNN........NN...NNN...........OOO.......OOO..........KKK....KKK............KKK....KKK...")
+    print("....NNN.........NN..NNN....**......OOO....OOO....**......KKK.....KKK.....**....KKK.....KKK..")
+    print("....NNN..........NN.NNN....**.........OOOO.......**......KKK.......KKK....**...KKK......KKK.")
 
+print()
 main()
+
+
+
